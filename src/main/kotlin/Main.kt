9d9a7282247bf195069d25d32eaf93bc499f3b53
MadJack1906 <cho.suhoond.74@gmail.com>
2022-03-30 15:09:27 +0800

    FizzBuzz implementation in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 fun main() {
-    var sampleDataObj: SampleDataClass = SampleDataClass("John Doe", 212, "Male")
-    println("Name is: ${sampleDataObj.name} \nAge is: ${sampleDataObj.age}\nGender is: ${sampleDataObj.gender}")
+    // FizzBuzz Implementation
+    for (i in 1..30){
+        if (i % 15 == 0){
+            println("FizzBuzz: $i")
+        } else if(i % 3 == 0){
+            println("Fizz: $i")
+        } else if (i % 5 == 0){
+            println("Buzz: $i")
+        } else {
+            println(i)
+        }
+    }
 }
