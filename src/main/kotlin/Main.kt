dceff087b46bb2f197ba1b1f19e3a50b4ef5a407
MadJack1906 <cho.suhoond.74@gmail.com>
2022-03-31 13:06:18 +0800

    Why everything has a value in Kotlin, the kotlin.Unit and also why everything is an expression in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,78 +1,14 @@
 import java.util.*
 
 fun main() {
-    // Immutable List
-    val fishList = listOf("Saint Peter's Fish", "Milk Fish", "Mudkip")
-    //println(fishList)
+    // Why everthing in kotlin has a value, kotlin.unit
+    val isUnit = println("Hello World")
+    val temperature = 10
+    val isHot = temperature > 50 // if (temperature > 50) true else false
 
-    // Mutable List
-    var mutableList = mutableListOf("Saint Peter's Fish", "Milk Fish", "Mudkip")
-    mutableList.remove("Milk Fish")
-    //println("Mutable List $mutableList")
+    println(isUnit)
+    println(isHot)
+    println("The current temperature in the Philippines is: ${if (temperature > 50) "it is hot" else "it is cold"}")
 
-    // Arrays
-    val arrayOne = arrayOf(1, 2, 3)
-    val arrayTwo = arrayOf(4, 5, 6)
-    val combinedArrays = arrayOne + arrayTwo
-    println(combinedArrays[5])
 
-    // Nested List
-    val numbers = intArrayOf(1,2,3)
-    val computerParts = listOf("Keyboard", "Monitor", "Mouse")
-    val nestedList = listOf(numbers, computerParts, "Water")
-    println(Arrays.toString(arrayOne))
-    println(nestedList)
-
-    // Nested List
-    val list = Array(5) { it * 5 }
-    println(list.contentToString())
-
-    // Loops
-    val school = arrayOf("shark", "salmon", "minnow")
-    // iterating through element
-    print("Iterating through element: ")
-    for(element in school){
-        print("$element ")
-    }
-    println()
-    // iterating with element and index
-    for((index, element) in school.withIndex()){
-        println("Item at $index is $element")
-    }
-
-    // Loops: Ranges and steps
-    // loop with range
-    for (i in 1..5){
-        print(i)
-    }
-    println()
-    // decrementing loop
-    for (i in 5 downTo 1){
-        print(i)
-    }
-    println()
-    // modified step incrementor/decrementor
-    for (i in 1..6 step 2){
-        print(i)
-    }
-    println()
-    // looping characters
-    for (i in 'g'..'m') print(i)
-    println()
-
-    // while loop
-    var bubbles = 0
-    while (bubbles < 10){
-        bubbles++
-    }
-
-    // do while loop
-    do{
-        bubbles--
-    }while (bubbles > 0)
-
-    // repeat
-    repeat(5) {
-        println("Print this: $it")
-    }
 }
