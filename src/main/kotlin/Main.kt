1218bfa5fa2c997c273adc26a46a579faee61b09
MadJack1906 <cho.suhoond.74@gmail.com>
2022-03-31 16:44:13 +0800

    Default Values, and Compact Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,13 +5,50 @@ private val randomDay = listOf("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FR
 
 fun main() {
     // Learn more about functions in Kotlin
-    println(feedTheFish())
+    // Default values and compact functions Kotlin
+
+    feedTheFish()
+}
+
+fun swim (speed: String = "fast"){
+    println("swimming speed is: $speed")
+}
+
+fun shouldChangeWaterCompact(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isDay(day) -> true
+        else -> false
+    }
+}
+
+// Compact Functions
+/*
+    Compact Functions are useful for making the code readable and also the logic
+ */
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isDay(day: String) = day == "Sunday"
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean{
+    return when{
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" -> true
+        else -> false
+    }
 }
 
 fun feedTheFish(){
-    val randomDay = randomDay()
-    val food = fishFood(randomDay)
+    val randomDay: String = randomDay()
+    val food: String = fishFood(randomDay)
+    //val shouldChangeWater = shouldChangeWater(randomDay)
     println("Today is $randomDay and the fish ate $food")
+    println("Change water: ${shouldChangeWater(randomDay)}")
+    println("Change water compact: ${shouldChangeWaterCompact(randomDay)}")
 }
 
 fun randomDay(): String {
